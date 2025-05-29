268d26246029f2d33b30ed1fec2cc53f3895de98
Paulina <fxcklleewyb@gmail.com>
2025-05-29 23:44:33 +0300

    chore: to change keyboard

--- constants/keyboards.py
+++ constants/keyboards.py
@@ -1,25 +1,41 @@
-admin_main_menu_keyboard = [
+main_menu_keyboard = [
     ['Ищу работу', 'region'],
     # ['Я студент', 'student'],
     ['О компании', 'about_company'],
-    # ['Частые вопросы', 'FQA'],
+    ['Частые вопросы', 'FQA'],
     ['Отправить анкету', 'user_data'],
     ['Контактная информация', 'contacts'],
-    ['Социальные сети', 'social_media'],
-    ['Панель администратора', 'admin_soft']
-]
+    # ['Социальные сети', 'social_media'],
+    ]
+
+user_main_menu_keyboard = main_menu_keyboard
+
+admin_main_menu_keyboard = main_menu_keyboard.copy()
+admin_main_menu_keyboard.append(['Панель администратора', 'admin_soft'])
+
+
+# admin_main_menu_keyboard = [
+#     ['Ищу работу', 'region'],
+#     # ['Я студент', 'student'],
+#     ['О компании', 'about_company'],
+#     # ['Частые вопросы', 'FQA'],
+#     ['Отправить анкету', 'user_data'],
+#     ['Контактная информация', 'contacts'],
+#     ['Социальные сети', 'social_media'],
+#     ['Панель администратора', 'admin_soft']
+# ]
 
 admin_options_keyboard = [["Рассылка"], ["Метрика"], ["Главное меню"]]
 
-user_main_menu_keyboard = [
-    ['Ищу работу', 'region'],
-    # ['Я студент', 'student'],
-    ['О компании', 'about_company'],
-    # ['Частые вопросы', 'FQA'],
-    ['Отправить анкету', 'user_data'],
-    ['Контактная информация', 'contacts'],
-    ['Социальные сети', 'social_media'],
-]
+# user_main_menu_keyboard = [
+#     ['Ищу работу', 'region'],
+#     # ['Я студент', 'student'],
+#     ['О компании', 'about_company'],
+#     # ['Частые вопросы', 'FQA'],
+#     ['Отправить анкету', 'user_data'],
+#     ['Контактная информация', 'contacts'],
+#     ['Социальные сети', 'social_media'],
+# ]
 
 vacancies_menu_keyboard = [
     ['Все вакансии'],
